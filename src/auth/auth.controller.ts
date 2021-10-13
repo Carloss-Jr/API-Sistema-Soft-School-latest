62400ce604aa01bb717f95810d6e3c77f9f53fe1
Carloss-Jr <carlos.gluckk@gmail.com>
2021-10-13 14:29:54 -0300

    add authResponse

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -3,12 +3,16 @@ import { Controller, Post, Req, UseGuards } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 import { AuthService } from './auth.service';
 import { ApiOkResponse } from '@nestjs/swagger';
+import { AuthResponse } from 'src/documentation/auth.response';
 
 @Controller('api/auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) { }
 
-  @ApiOkResponse()
+  @ApiOkResponse({
+    type: AuthResponse
+  }
+  )
   @UseGuards(AuthGuard('local'))
   @Post('login')
   async login(@Req() req: any) {
